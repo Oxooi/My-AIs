1f86cb2a1fdb632ab12683a48d4b9504921a563d
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-06-12 03:04:32 +0200

    Finish

--- Python/Flappy_Bird/flappy.py
+++ Python/Flappy_Bird/flappy.py
@@ -8,6 +8,8 @@ pygame.font.init()
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
+GEN = 0
+
 BIRD_IMGS = [
     pygame.transform.scale2x(
         pygame.image.load(os.path.join("imgs", "bird1.png"))
@@ -169,17 +171,27 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0, 0))
 
     for pipe in pipes:
         pipe.draw(win)
 
+    # score
     text = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
-    base.draw(win)
+    # gen
+    text = STAT_FONT.render("Tries: " + str(gen), 1, (255, 255, 255))
+    win.blit(text, (10, 10))
+
+    # alive
+    score_label = STAT_FONT.render(
+        "Alive: " + str(len(birds)), 1, (255, 255, 255))
+    win.blit(score_label, (10, 50))
     
+    base.draw(win)
+
     for bird in birds:
         bird.draw(win)
 
@@ -187,6 +199,8 @@ def draw_window(win, birds, pipes, base, score):
 
 
 def main(genomes, config):
+    global GEN
+    GEN += 1
     nets = []
     ge = []
     birds = []
@@ -267,7 +281,7 @@ def main(genomes, config):
                 ge.pop(x)
 
         base.move()
-        draw_window(win, birds, pipes, base, score)
+        draw_window(win, birds, pipes, base, score, GEN)
 
 
 def run(cofig_path):
