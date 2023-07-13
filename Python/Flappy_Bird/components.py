9a9a550e4ca524ad98efa9556fdc72fead70da42
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-07-13 15:39:28 +0200

    Add option to switch the textures

--- Python/Flappy_Bird/components.py
+++ Python/Flappy_Bird/components.py
@@ -7,22 +7,29 @@ WIN_HEIGHT = 800
 
 GEN = 0
 
-BIRD_IMGS = [
-    pygame.transform.scale2x(
-        pygame.image.load(os.path.join("imgs", "bird1.png"))
-    ),
-    pygame.transform.scale2x(
-        pygame.image.load(os.path.join("imgs", "bird2.png"))
-    ),
-    pygame.transform.scale2x(
-        pygame.image.load(os.path.join("imgs", "bird3.png"))
-    ),
-]
-PIPE_IMG = pygame.transform.scale2x(
-    pygame.image.load(os.path.join("imgs", "pipe.png")))
-BASE_IMG = pygame.transform.scale2x(
-    pygame.image.load(os.path.join("imgs", "base.png")))
-BG_IMG = pygame.transform.scale2x(
-    pygame.image.load(os.path.join("imgs", "bg.png")))
+textures_folders = ["minecraft"]
+
+BIRD_IMGS = []
+
+for folder in textures_folders:
+    # Chemin du dossier
+    folder_path = os.path.join("imgs", folder)
+    
+    # Chargement et mise à l'échelle des images de l'oiseau
+    for i in range(1, 4):  # Les numéros des images vont de 1 à 3
+        image_path = os.path.join(folder_path, f"bird{i}.png")
+        bird_img = pygame.transform.scale2x(pygame.image.load(image_path))
+        BIRD_IMGS.append(bird_img)
+
+    # Chargement des autres images
+    PIPE_IMG = pygame.transform.scale2x(
+        pygame.image.load(os.path.join(folder_path, "pipe.png"))
+    )
+    BASE_IMG = pygame.transform.scale2x(
+        pygame.image.load(os.path.join(folder_path, "base.png"))
+    )
+    BG_IMG = pygame.transform.scale2x(
+        pygame.image.load(os.path.join(folder_path, "bg.png"))
+    )
 
 STAT_FONT = pygame.font.SysFont("impact", 40)
